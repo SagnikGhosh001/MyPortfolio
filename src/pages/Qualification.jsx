eedbfa29df45328135b5215eb7cab0ba3db4cda5
SagnikGhosh001 <sagnikghosh904@gmail.com>
2024-09-16 21:39:07 +0530

    ui fix

--- src/pages/Qualification.jsx
+++ src/pages/Qualification.jsx
@@ -4,13 +4,25 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import DownloadIcon from '@mui/icons-material/Download';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import { Link } from 'react-router-dom';
 function Qualification() {
+    const certificate_URL = 'https://sagnikghosh.netlify.app/Internship_Certificate.png';
     const [value, setValue] = React.useState('1');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    };
+
+    const downloadCertificate = (url) => {
+        const fileName = url.split('/').pop();
+        const aTag = document.createElement('a');
+        aTag.href = url;
+        aTag.setAttribute('download', fileName);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
     };
 
     return (
@@ -176,13 +188,80 @@ function Qualification() {
                         YCSAS Pvt. Ltd
                     </Typography>
                     <Typography variant="h6" sx={{ marginBottom: '10px' }}>
-                        internship
+                        Current Internship (Sep. 20,2024 to Present)
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Gained experience through the internship at YSS Company, covering a comprehensive curriculum including:
+
+                        <strong>Front End Core Skills:</strong>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>MUI</li>
+                        </ul>
+
+                        <strong>Additional Tools & Concepts:</strong>
+                        <ul>
+                            <li>Confluence (Basic)</li>
+                            <li>Jira (Basic)</li>
+                            <li>Figma (Basic)</li>
+                            <li>Excalidraw/Draw.io (Basic)</li>
+                            <li>Git/GitHub (Basic)</li>
+                        </ul>
+
+                        <strong>Back End Core Skills:</strong>
+                        <ul>
+                            <li>Python</li>
+                            <li>Django (Advanced)</li>
+                            <li>MySQL</li>
+                        </ul>
+                    </Typography>
+                </TabPanel>
+                <TabPanel
+                    value="2"
+                    sx={{
+                        padding: '20px',
+                        backgroundColor: '#ffffff',
+                        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                        borderRadius: '8px',
+                        marginTop: '20px',
+                        maxWidth: '600px',
+                        width: '100%',
+                    }}
+                >
+
+                    <Typography variant="h4" sx={{ marginBottom: '10px' }}>
+                        YCSAS Pvt. Ltd
+                    </Typography>
+                    <Typography variant="h6" sx={{ marginBottom: '10px' }}>
+                        Internship in Web Development (Nov. 1,2023 to Dec. 15,2023)
+
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         To leverage my experience gained through the internship at YSS Company, where I acquired practical skills in frontend and backend development,
                         including HTML, CSS, Django, Bootstrap, and DBMS.
                     </Typography>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<DownloadIcon />}
+                        sx={{
+                            marginBottom: '15px',
+                            marginTop: '20px',
+                            size: 'large',
+                            backgroundColor: '#1976d2',
+                            '&:hover': {
+                                backgroundColor: '#115293'
+                            }
+                        }}
+                        href="path/to/your/certificate.pdf"
+                        download
+                        onClick={() => downloadCertificate(certificate_URL)}
+                    >
+                        Download Certificate
+                    </Button>
                 </TabPanel>
+
             </TabContext>
             <Typography
                 gutterBottom
